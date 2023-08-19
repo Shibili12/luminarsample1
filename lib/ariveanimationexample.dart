58d81511425676e1dcb9cfa850ec4413792ee4cb
shibili12 <shibiliazwin23@gmail.com>
2023-08-19 11:16:02 +0530

    commit

---
+++ lib/ariveanimationexample.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Riveexample(),
+  ));
+}
+
+class Riveexample extends StatelessWidget {
+  const Riveexample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: RiveAnimation.asset("assets/animation/3_-_truck.riv"),
+      ),
+    );
+  }
+}
